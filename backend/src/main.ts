d853b4c7796140c0a98244c11b28e3a566f0803a
unknown <vandat14040608@gmail.com>
2026-06-18 00:06:17 +0700

    update docker file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 8000);
-  console.log(`Application is running on: await app.getUrl()`);
+  await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
